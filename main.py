4bf6219477876092a66532e8e15f4d371e1d156b
Computer <96786190+PiKa919@users.noreply.github.com>
2023-09-29 16:24:11 +0530

    added comments

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import streamlit as st
 import openai
 
+#Initialize OpenAI
 api_key = "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"
 model_id = "gpt-3.5-turbo"
 
+#Defining the function to generate the article
 def generate_article(keywords, writing_style, words):
     return "This is a test article generated"
 
+#Defining the app
 keyword = st.text_input("Enter keywords")
 writing_style = st.selectbox("Select writing style", ["Formal", "Casual", "Friendly", "Humorous", "Poetic"])  
 words = st.slider("Number of words", min_value=100, max_value=1000, value=300)
 
+#Generate article
 submit = st.button("Generate")
 
+#Display article
 if submit:
     message = st.empty()
     message.text("Generating article...")
@@ -20,4 +25,5 @@ if submit:
     message.text("Article generated!")
     st.write(article)
     
+    #Download article
     st.download_button(label="Download article", data=article, file_name="article.txt", mime="text/txt")
